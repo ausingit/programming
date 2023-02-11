f003d96dcae15f896bc6351f8243621ee44484fb
Utkarsh <gitboyrex@gmail.com>
2023-02-12 03:38:19 +0530

    Initial Commit

---
+++ rust/target/debug/deps/rust-bd1c35ca977540e6.d
@@ -0,0 +1,5 @@
+/home/utkarsh/Desktop/pl/rust/target/debug/deps/rust-bd1c35ca977540e6: src/main.rs
+
+/home/utkarsh/Desktop/pl/rust/target/debug/deps/rust-bd1c35ca977540e6.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+/home/utkarsh/Desktop/pl/rust/target/debug/rust: /home/utkarsh/Desktop/pl/rust/src/main.rs
